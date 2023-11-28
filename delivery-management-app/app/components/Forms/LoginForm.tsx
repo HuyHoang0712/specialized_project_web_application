991609347f8fd6f85df6463cef2819735f721c0c
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2023-11-28 14:48:33 +0700

    Basic UI done!

--- delivery-management-app/app/components/Forms/LoginForm.tsx
+++ delivery-management-app/app/components/Forms/LoginForm.tsx
@@ -4,21 +4,32 @@ import RequiredInput from "../Inputs/RequiredInput";
 import Image from "next/image";
 function LoginForm() {
   const userNameInput = {
-    placeholder: 'Username',
-    type: 'text',
+    placeholder: "Username",
+    type: "text",
     required: true,
-    icon: 'Mail'
-  }
+    icon: "Mail",
+  };
   const passwordInput = {
-    placeholder: 'Password',
-    type: 'password',
+    placeholder: "Password",
+    type: "password",
     required: true,
-    icon: 'Password'
-  }
+    icon: "Password",
+  };
   return (
-    <div className="flex flex-col items-center justify-start gap-8 w-full">
-      <RequiredInput {...userNameInput}/>
-      <RequiredInput {...passwordInput}/>
+    <div className="flex flex-col items-center justify-start w-full">
+      <div className="flex flex-col items-center justify-start gap-8 w-full">
+        <RequiredInput {...userNameInput} />
+        <RequiredInput {...passwordInput} />
+      </div>
+      <span
+        className="p2 text-primary-100 font-['Poppins-Regular'] mt-3 self-end"
+        onClick={() => {}}
+      >
+        Recover Password
+      </span>
+      <button
+        className="rounded-xl w-[11.25rem] bg-primary-100 p-3 font-['Poppins-Regular'] text-white text-xl mt-12"
+      >Login</button>
     </div>
   );
 }
