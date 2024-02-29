b8022e2bca57aea66d5e473a6dd586d657ca9845
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-02-29 21:18:57 +0700

    feat: Order Management Main Page done!

---
+++ delivery-management-app/app/components/Lists/List.tsx
@@ -0,0 +1,118 @@
+"use client";
+import React, { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+
+interface Props {
+  headers: string[];
+  data: any[];
+}
+
+const List = (props: Props) => {
+  const { headers, data } = props;
+  const lenData = data.length;
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [totalPage, setTotalPage] = useState(Math.ceil(lenData / itemsPerPage));
+  const [curPage, setCurPage] = useState(1);
+  const [curData, setCurData] = useState(data.slice(0, itemsPerPage));
+
+  const movePage = (move: number) => {
+    if (move > 0 && curPage < totalPage) {
+      setCurData(
+        data.slice(
+          itemsPerPage * move,
+          curPage === totalPage ? lenData : itemsPerPage * (move + 1)
+        )
+      );
+      setCurPage(curPage + 1);
+    } else if (move < 0 && curPage > 1) {
+      setCurData(
+        data.slice(
+          itemsPerPage * (curPage - 1 + move),
+          itemsPerPage * (curPage + move)
+        )
+      );
+      setCurPage(curPage - 1);
+    }
+  };
+
+  const updateItemsPerPage = (items: number) => {
+    let newTotalPage = Math.ceil(lenData / items);
+    if (curPage >= newTotalPage) {
+      setCurData(data.slice(itemsPerPage * (newTotalPage - 1), lenData));
+      setCurPage(newTotalPage);
+    } else
+      setCurData(
+        data.slice(itemsPerPage * (curPage - 1), itemsPerPage * curPage)
+      );
+    setTotalPage(newTotalPage);
+    setItemsPerPage(items);
+  };
+
+  return (
+    <div className="flex h-[90%] flex-col">
+      <div className="flex items-center p-3 border-y-2 font-medium text-black-90">
+        {headers.map((item, idx) => (
+          <span key={idx} className="flex flex-1">
+            {item}
+          </span>
+        ))}
+      </div>
+      <div className="flex flex-1 flex-col p-3 gap-6 text-black-50 overflow-auto no-scrollbar">
+        {curData.map((item, idx) => (
+          <div key={idx} className="flex items-center">
+            {Object.values(item).map((value: any, index) => (
+              <span key={index} className="flex flex-1">
+                {value}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+      <div className="flex px-3 py-2 justify-between border-t-2 text-sm font-medium">
+        <div className="flex gap-2 items-center">
+          <select
+            id="items"
+            className="min-w-[3.75rem] px-3 py-1 text-xs rounded-lg border-none text-black-40 bg-gray-100"
+            onChange={(e) => updateItemsPerPage(parseInt(e.target.value))}
+          >
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={30}>30</option>
+          </select>
+          <label htmlFor="items" className="text-black-40">
+            Items per Page
+          </label>
+          <span className="flex items-center text-black-50 ml-5">
+            1-{itemsPerPage} of {lenData} items
+          </span>
+        </div>
+        <div className="flex gap-2 items-center">
+          <input
+            id="items"
+            type="number"
+            min={1}
+            max={totalPage}
+            className="w-[3.125rem] px-3 py-1 text-xs rounded-lg border-none text-black-40 bg-gray-100"
+            value={curPage}
+            onChange={(e) => movePage(parseInt(e.target.value) - curPage)}
+          />
+          <label htmlFor="items" className="text-black-40">
+            of {totalPage} pages
+          </label>
+          <span className="flex items-center ml-2 gap-2 text-black-50">
+            <ChevronLeftIcon
+              className="w-4 icon-sw-2 hover:text-primary-100"
+              onClick={() => movePage(-1)}
+            />
+            <ChevronRightIcon
+              className="w-4 icon-sw-2 hover:text-primary-100"
+              onClick={() => movePage(1)}
+            />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- delivery-management-app/app/components/Lists/TransportationPlanList.tsx
+++ delivery-management-app/app/components/Lists/TransportationPlanList.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Search from "../Search/Search";
 import FilterModal from "../Modals/FilterModal";
+import List from "./List";
 
 const transportationPlanList = [
   {
@@ -96,9 +97,14 @@ const transportationPlanList = [
   },
 ];
 
+const listProps = {
+  headers: ["Date", "Number of Orders", "Status", "Issue"],
+  data: transportationPlanList,
+};
+
 const TransportationPlanList = () => {
   return (
-    <div className="flex flex-1 flex-col gap-3">
+    <div className="flex h-full w-full flex-col gap-3">
       <div className="flex flex-row w-full justify-between items-center">
         <span className="text-lg text-black-60 font-medium">
           Transportation Plans
@@ -108,24 +114,7 @@ const TransportationPlanList = () => {
           <FilterModal />
         </div>
       </div>
-      <div className="list-content">
-        <div className="list-header">
-          <span>Date</span>
-          <span>Number of Orders</span>
-          <span>Status</span>
-          <span>issues</span>
-        </div>
-        <div className="list-body no-scrollbar">
-          {transportationPlanList.map((item, idx) => (
-            <div key={idx} className="body-row">
-              <span>{item.date}</span>
-              <span>{item.orders}</span>
-              <span>{item.status}</span>
-              <span>{item.issues}</span>
-            </div>
-          ))}
-        </div>
-      </div>
+      <List {...listProps} />
     </div>
   );
 };
