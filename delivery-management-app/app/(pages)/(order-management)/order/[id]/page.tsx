465c687f7870aed35299ae2f18725e8a7f08b0a7
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-03-10 15:13:34 +0700

    feat: adding icon for orderinforcard

--- delivery-management-app/app/(pages)/(order-management)/order/[id]/page.tsx
+++ delivery-management-app/app/(pages)/(order-management)/order/[id]/page.tsx
@@ -3,15 +3,21 @@ import CancelOrderModal from "@/app/components/Modals/CancelOrderModal";
 import ReportIssueModal from "@/app/components/Modals/ReportIssueModal";
 import OrderInforCard from "@/app/components/Cards/OrderInforCard";
 
-
 const OrderDetailPage = ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
   const driver_card_props = {
     type: "driver",
-    title: "Assigned Driver",
-    titleContent: { name: "Furuga Rei", driver_id: "01224" },
-    content: { phone: "+8490099129", email: "example@gmail.com" },
+    title: "Driver",
+    titleContent: { Name: "Furuga Rei", "Driver ID": "01224" },
+    content: { Phone: "+8490099129", Email: "example@gmail.com" },
+  };
+
+  const vehicle_card_props = {
+    type: "vehicle",
+    title: "Vehicle",
+    titleContent: { "License Plate": "51C-12304", Capacity: "7500 kg" },
+    content: { "Current Parking": "No. 15 Adekunle Street, Yaba, Lagos State" },
   };
 
   return (
@@ -21,7 +27,9 @@ const OrderDetailPage = ({ params }: { params: { id: string } }) => {
         <CancelOrderModal />
       </div>
       <div className="flex w-full gap-3">
-        <OrderInforCard {...driver_card_props}/>
+        <OrderInforCard {...driver_card_props} />
+        <OrderInforCard {...vehicle_card_props} />
+        <OrderInforCard {...vehicle_card_props} />
       </div>
     </div>
   );
--- delivery-management-app/app/components/Cards/OrderInforCard.tsx
+++ delivery-management-app/app/components/Cards/OrderInforCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
-
 interface Prosp {
   type: string;
   title: string;
@@ -22,14 +21,99 @@ const OrderInforCard = (props: Prosp) => {
         }
       );
       break;
+    case "vehicle":
+      Icon = dynamic(
+        () => import("@heroicons/react/24/solid").then((res) => res.TruckIcon),
+        {
+          ssr: false,
+          loading: () => <>Loading...</>,
+        }
+      );
+      break;
 
     default:
+      Icon = dynamic(
+        () =>
+          import("@heroicons/react/24/solid").then(
+            (res) => res.QuestionMarkCircleIcon
+          ),
+        {
+          ssr: false,
+          loading: () => <>Loading...</>,
+        }
+      );
       break;
   }
 
+  const ContentIcon = Object.keys(content).map((key) => {
+    switch (key) {
+      case "Phone":
+        return dynamic(
+          () =>
+            import("@heroicons/react/24/solid").then((res) => res.PhoneIcon),
+          {
+            ssr: false,
+            loading: () => <>Loading...</>,
+          }
+        );
+      case "Email":
+        return dynamic(
+          () =>
+            import("@heroicons/react/24/solid").then((res) => res.EnvelopeIcon),
+          {
+            ssr: false,
+            loading: () => <>Loading...</>,
+          }
+        );
+
+      default:
+        return dynamic(
+          () =>
+            import("@heroicons/react/24/solid").then(
+              (res) => res.QuestionMarkCircleIcon
+            ),
+          {
+            ssr: false,
+            loading: () => <>Loading...</>,
+          }
+        );
+    }
+  });
+
+  const getIcon = (idx: number) => {
+    let CurIcon = ContentIcon[idx];
+    return <CurIcon className="w-[0.875rem]" />;
+  };
+
   return (
-    <div className="bg-white rounded-lg p-3">
-      <Icon className="w-5 bg-secondary-20 rounded-lg" />
+    <div className="flex flex-1 flex-col w-[25rem] bg-white rounded-lg p-3 gap-6">
+      <div className="flex items-start gap-6">
+        <Icon className="w-10 p-2 bg-secondary-20 rounded-lg" />
+        <div className="flex flex-col">
+          <span className="text-base font-bold text-black-90">{title}</span>
+          {Object.entries(titleContent).map(
+            ([key, value]: [string, any], idx) => (
+              <span className="text-sm font-medium text-black-60" key={idx}>
+                <span className="text-black-30">{key}:</span> {value}
+              </span>
+            )
+          )}
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        {Object.entries(content).map(([key, value]: [string, any], idx) => (
+          <div
+            className="flex flex-col text-sm font-medium text-black-60"
+            key={idx}
+          >
+            <span className="flex gap-1 text-black-30">
+              {getIcon(idx)}
+              {key}
+            </span>
+            {value}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
