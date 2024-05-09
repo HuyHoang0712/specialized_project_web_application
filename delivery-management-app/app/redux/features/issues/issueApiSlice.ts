c01987e0182688ec4efc0b09a2a07d5fe9b983ab
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-05-09 23:25:24 +0700

    resolve conflict

--- delivery-management-app/app/redux/features/issues/issueApiSlice.ts
+++ delivery-management-app/app/redux/features/issues/issueApiSlice.ts
@@ -17,16 +17,20 @@ export const issueApiSlice = apiSlice.injectEndpoints({
       query: (type) => URLS.ISSUE_URL + `get_issues/?type=${type}`,
     }),
     getIssuesByStatsus: builder.query({
-      query: (data: number) => URLS.ISSUE_URL + `get_issues_by_status?status=${data}`,
+      query: (data: number) =>
+        URLS.ISSUE_URL + `get_issues_by_status?status=${data}`,
     }),
     getIssuesByEmployeeId: builder.query({
-      query: (data: string) => URLS.ISSUE_URL + `get_issues_by_employee_id?employee_id=${data}`,
+      query: (data: string) =>
+        URLS.ISSUE_URL + `get_issues_by_employee_id?employee_id=${data}`,
     }),
     getIssuesOfVehicle: builder.query({
-      query: (data: string) => URLS.ISSUE_URL + `get_issues_of_vehicle?vehicle=${data}`,
+      query: (data: string) =>
+        URLS.ISSUE_URL + `get_issues_of_vehicle?vehicle=${data}`,
     }),
     getIssueById: builder.query({
-      query: ({ id, type }) => URLS.ISSUE_URL + `get_issue_by_id/?id=${id}&type=${type}`,
+      query: ({ id, type }) =>
+        URLS.ISSUE_URL + `get_issue_by_id/?id=${id}&type=${type}`,
     }),
     getCurrentEmployeeIssues: builder.query({
       query: () => URLS.ISSUE_URL + `get_user_issues`,
@@ -34,18 +38,27 @@ export const issueApiSlice = apiSlice.injectEndpoints({
     updateIssueStatus: builder.mutation({
       query: (data: { id: string; status: number; type: string }) => ({
         headers: { "Content-Type": "application/json" },
-        url: URLS.ISSUE_URL + `update_issue_status/?id=${data.id}&type=${data.type}`,
+        url:
+          URLS.ISSUE_URL +
+          `update_issue_status/?id=${data.id}&type=${data.type}`,
         method: "PUT",
         body: JSON.stringify(data),
       }),
-      onQueryStarted: async ({ id, type, ...put }, { dispatch, queryFulfilled }) => {
+      onQueryStarted: async (
+        { id, type, ...put },
+        { dispatch, queryFulfilled }
+      ) => {
         try {
           const res = await queryFulfilled;
 
           dispatch(
-            issueApiSlice.util.updateQueryData("getIssueById", { id, type }, (draft) => {
-              Object.assign(draft, res.data);
-            })
+            issueApiSlice.util.updateQueryData(
+              "getIssueById",
+              { id, type },
+              (draft) => {
+                Object.assign(draft, res.data);
+              }
+            )
           );
           dispatch(
             issueApiSlice.util.updateQueryData("getAllIssue", type, (draft) => {
@@ -57,13 +70,17 @@ export const issueApiSlice = apiSlice.injectEndpoints({
             })
           );
           dispatch(
-            issueApiSlice.util.updateQueryData("getCurrentEmployeeIssues", undefined, (draft) => {
-              draft.map((item: any) => {
-                if (item.id === id) {
-                  item.status = put.status;
-                }
-              });
-            })
+            issueApiSlice.util.updateQueryData(
+              "getCurrentEmployeeIssues",
+              undefined,
+              (draft) => {
+                draft.map((item: any) => {
+                  if (item.id === id) {
+                    item.status = put.status;
+                  }
+                });
+              }
+            )
           );
         } catch (error) {
           throw error;
@@ -73,4 +90,13 @@ export const issueApiSlice = apiSlice.injectEndpoints({
   }),
 });
 
-export const { useGetAllIssueQuery, useGetIssuesByStatsusQuery, useGetIssuesByEmployeeIdQuery, useGetIssuesOfVehicleQuery, useGetIssueByIdQuery, useUpdateIssueStatusMutation, useGetCurrentEmployeeIssuesQuery, useCreateIssueMutation } = issueApiSlice;
+export const {
+  useGetAllIssueQuery,
+  useGetIssuesByStatsusQuery,
+  useGetIssuesByEmployeeIdQuery,
+  useGetIssuesOfVehicleQuery,
+  useGetIssueByIdQuery,
+  useUpdateIssueStatusMutation,
+  useGetCurrentEmployeeIssuesQuery,
+  useCreateIssueMutation,
+} = issueApiSlice;
